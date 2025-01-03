456a11d5cf2911423fd38a954d509367f69bfb92
hcratch3 <162408146+hcratch3@users.noreply.github.com>
2025-01-03 18:12:53 +0900

    Update blocks-only.jsx

--- src/playground/blocks-only.jsx
+++ src/playground/blocks-only.jsx
@@ -15,8 +15,6 @@ const mapStateToProps = state => ({vm: state.scratchGui.vm});
 const VMBlocks = connect(mapStateToProps)(Blocks);
 const VMControls = connect(mapStateToProps)(Controls);
 
-import addExtensionIcon from '../components/gui/icon--extensions.svg';
-
 const BlocksOnly = props => (
     <GUI {...props}>
         <VMBlocks
@@ -25,19 +23,6 @@ const BlocksOnly = props => (
                 media: `static/blocks-media/default/`
             }}
         />
-        <Box className={styles.extensionButtonContainer}>
-          <button
-            className={styles.extensionButton}
-            title={intl.formatMessage(messages.addExtension)}
-            onClick={onExtensionButtonClick}
-          >
-            <img
-              className={styles.extensionButtonIcon}
-              draggable={false}
-              src={addExtensionIcon}
-            />
-          </button>
-        </Box>
         <VMControls className={styles.controls} />
     </GUI>
 );
