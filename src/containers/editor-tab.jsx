f6e482f1506ef9fd47bfdc03632f480009f89d54
hcratch3 <162408146+hcratch3@users.noreply.github.com>
2025-01-04 10:31:51 +0900

    Update editor-tab.jsx

--- src/containers/editor-tab.jsx
+++ src/containers/editor-tab.jsx
@@ -1,91 +1,131 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import bindAll from 'lodash.bindall';
-import {defineMessages, intlShape, injectIntl} from 'react-intl';
+import {connect} from 'react-redux';
 import VM from 'scratch-vm';
 
-import AssetPanel from '../components/asset-panel/asset-panel.jsx';
-
-import {connect} from 'react-redux';
-
-import {
-    activateTab,
-    COSTUMES_TAB_INDEX
-} from '../reducers/editor-tab';
-
 import {setRestore} from '../reducers/restore-deletion';
-import {showStandardAlert, closeAlertWithId} from '../reducers/alerts';
+import errorBoundaryHOC from '../lib/error-boundary-hoc.jsx';
 
 class EditorTab extends React.Component {
-    render () {
-        const {
-            vm
-        } = this.props;
+    constructor(props) {
+        super(props);
+        bindAll(this, [
+            'handleCodeChange',
+            'handleSpriteChange'
+        ]);
 
-        if (!vm.editingTarget) {
-            return null;
+        // スプライトごとのコードを保持
+        this.state = {
+            spriteCode: this.initializeSpriteCode(props.sprites, props.stage),
+            selectedSpriteId: props.editingTarget // 現在選択されているスプライトID
+        };
+    }
+
+    componentWillReceiveProps(nextProps) {
+        // スプライトが追加された場合に初期化
+        if (Object.keys(nextProps.sprites).length !== Object.keys(this.props.sprites).length) {
+            this.setState({
+                spriteCode: this.initializeSpriteCode(nextProps.sprites, nextProps.stage)
+            });
         }
 
-        const messages = defineMessages({
-            fileUploadSound: {
-                defaultMessage: 'Upload Sound',
-                description: 'Button to upload sound from file in the editor tab',
-                id: 'gui.soundTab.fileUploadSound'
-            },
-            surpriseSound: {
-                defaultMessage: 'Surprise',
-                description: 'Button to get a random sound in the editor tab',
-                id: 'gui.soundTab.surpriseSound'
-            },
-            recordSound: {
-                defaultMessage: 'Record',
-                description: 'Button to record a sound in the editor tab',
-                id: 'gui.soundTab.recordSound'
-            },
-            addSound: {
-                defaultMessage: 'Choose a Sound',
-                description: 'Button to add a sound in the editor tab',
-                id: 'gui.soundTab.addSoundFromLibrary'
+        // 編集対象スプライトが変わった場合に選択を更新
+        if (nextProps.editingTarget !== this.props.editingTarget) {
+            this.setState({selectedSpriteId: nextProps.editingTarget});
+        }
+    }
+
+    initializeSpriteCode(sprites, stage) {
+        const codeMap = {};
+        // スプライトとステージの初期コードを空文字列で設定
+        Object.keys(sprites).forEach(spriteId => {
+            codeMap[spriteId] = '';
+        });
+        if (stage) {
+            codeMap[stage.id] = '';
+        }
+        return codeMap;
+    }
+
+    handleCodeChange(event) {
+        const newCode = event.target.value;
+        const {selectedSpriteId, spriteCode} = this.state;
+
+        // 選択中のスプライトのコードを更新
+        this.setState({
+            spriteCode: {
+                ...spriteCode,
+                [selectedSpriteId]: newCode
             }
         });
+    }
+
+    handleSpriteChange(spriteId) {
+        // スプライトの切り替え
+        this.setState({selectedSpriteId: spriteId});
+    }
+
+    render() {
+        const {spriteCode, selectedSpriteId} = this.state;
+        const {sprites, stage} = this.props;
+
+        const currentCode = spriteCode[selectedSpriteId] || '';
 
         return (
-            <AssetPanel>
-                <div>Hello</div>
-            </AssetPanel>
+            <div className="code-editor-tab">
+                <div className="sprite-selector">
+                    <h3>Sprites</h3>
+                    <ul>
+                        {/* スプライトのリストを表示 */}
+                        {Object.keys(sprites).map(spriteId => (
+                            <li
+                                key={spriteId}
+                                className={spriteId === selectedSpriteId ? 'selected' : ''}
+                                onClick={() => this.handleSpriteChange(spriteId)}
+                            >
+                                {sprites[spriteId].name}
+                            </li>
+                        ))}
+                        {/* ステージも選択可能 */}
+                        {stage && (
+                            <li
+                                key={stage.id}
+                                className={stage.id === selectedSpriteId ? 'selected' : ''}
+                                onClick={() => this.handleSpriteChange(stage.id)}
+                            >
+                                Stage
+                            </li>
+                        )}
+                    </ul>
+                </div>
+                <div className="code-editor">
+                    <h3>Code Editor</h3>
+                    <textarea
+                        value={currentCode}
+                        onChange={this.handleCodeChange}
+                        placeholder="Write your code here..."
+                    />
+                </div>
+            </div>
         );
     }
 }
 
 EditorTab.propTypes = {
-    onActivateEditorTab: PropTypes.func.isRequired,
-    sprites: PropTypes.shape({
-        id: PropTypes.shape({
-            sounds: PropTypes.arrayOf(PropTypes.shape({
-                name: PropTypes.string.isRequired
-            }))
-        })
-    }),
-    stage: PropTypes.shape({
-        sounds: PropTypes.arrayOf(PropTypes.shape({
-            name: PropTypes.string.isRequired
-        }))
-    }),
+    editingTarget: PropTypes.string,
+    sprites: PropTypes.object.isRequired,
+    stage: PropTypes.object,
     vm: PropTypes.instanceOf(VM).isRequired
 };
 
 const mapStateToProps = state => ({
+    editingTarget: state.scratchGui.targets.editingTarget,
     sprites: state.scratchGui.targets.sprites,
     stage: state.scratchGui.targets.stage,
+    vm: state.scratchGui.vm
 });
 
-const mapDispatchToProps = dispatch => ({
-    onActivateEditorTab: () => dispatch(activateTab(COSTUMES_TAB_INDEX)),
-});
-
-export default errorBoundaryHOC('Editor Tab')(
-    injectIntl(connect(
-        mapStateToProps,
-        mapDispatchToProps
-    )(EditorTab))
+export default errorBoundaryHOC('CodeEditorTab')(
+    connect(mapStateToProps)(CodeEditorTab)
 );
